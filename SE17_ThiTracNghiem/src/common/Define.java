fe68e3eaa148ea923b51422059fc9f9f36460c18
DESKTOP-6VQ20DT\Uyen Cheri <honguyen.cnttk37b@gmail.com>
2017-04-04 00:13:28 +0700

    xong

--- SE17_ThiTracNghiem/src/common/Define.java
+++
@@ -1,8 +0,0 @@
-package common;
-
-public class Define {
-	public static String SEVER_NAME = "localhost";
-	public static String DB_NAME = "TracNghiemOnline";
-	public static String DB_USERNAME = "sa";
-	public static String DB_PASSWORD = "123";
-}
